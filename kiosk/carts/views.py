e810f9d3d0d8e69263aadb650e948ce033e74916
peter mutunga <peter.mutunga@oceanscan.co.ke>
2023-06-08 23:58:05 +0300

    store and carts functionalities added

--- kiosk/carts/views.py
+++ kiosk/carts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import *
+from store.models import Product, Variation
 # Create your views here.
 from django.http import HttpResponse 
 from django.core.exceptions import ObjectDoesNotExist
@@ -11,6 +12,16 @@ def _cart_id(request):
 
 def add_cart(request, product_id):
     product = Product.objects.get(id=product_id)
+    product_variation = []
+    if request.method == "POST":
+        for item in request.POST:
+            key = item
+            value = request.POST[key]
+            try:
+                variation = Variation.objects.get(product=product, variation_category__iexact=key, variation_value__iexact=value)
+                product_variation.append(variation)
+            except:
+                 pass
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request)) #get the cart using the cart_id present in the session
     except Cart.DoesNotExist:
@@ -18,36 +29,67 @@ def add_cart(request, product_id):
             cart_id=_cart_id(request)
         )
     cart.save()
-       
-    try:
-            cart_item = CartItem.objects.get(product=product, cart=cart)
-            cart_item.quantity += 1 #cart_item quantity
-            cart_item.save()
-    except CartItem.DoesNotExist:
+    
+    is_cart_item_exists = CartItem.objects.filter(product=product, cart=cart).exists()
+    if is_cart_item_exists:
+            cart_item = CartItem.objects.filter(product=product, cart=cart)
+            # existing variations -> database
+            # current variations -> product_variation
+            # item_id -> database 
+
+            ex_var_list = []
+            id = []
+            for item in cart_item:
+                 existing_variation = item.variations.all()
+                 ex_var_list.append(list(existing_variation))
+                 id.append(item.id)
+            print(ex_var_list)
+
+            if product_variation in ex_var_list:
+                 # increase cart_item quantity
+                 index = ex_var_list.index(product_variation)
+                 item_id = id[index]
+                 item = CartItem.objects.get(product=product, id=item_id)
+                 item.quantity += 1
+                 item.save()
+            else: 
+                item = CartItem.objects.create(product=product, quantity =1, cart=cart)
+                if len(product_variation) > 0:
+                 item.variations.clear()
+                 item.variations.add(*product_variation)
+            # cart_item.quantity += 1 #cart_item quantity
+                 item.save()
+    else:
             cart_item = CartItem.objects.create(
                  product=product,   
                  quantity = 1,
                  cart = cart
             )
+            if len(product_variation) > 0:
+                 cart_item.variations.clear()
+                 cart_item.variations.add(*product_variation)
             cart_item.save()
     # return HttpResponse(cart_item.product)
     return redirect('cart')
-def remove_cart(request, product_id):
+def remove_cart(request, product_id, cart_item_id):
      cart = Cart.objects.get(cart_id=_cart_id(request))
     #  product = Product.objects.get(id=product_id)
      product = get_object_or_404(Product, id=product_id)
-     cart_item = CartItem.objects.get(product=product, cart=cart)
-     if cart_item.quantity > 1:
-        cart_item.quantity -= 1
-        cart_item.save()
-     else:
-            cart_item.delete()
+     try:
+            cart_item = CartItem.objects.get(product=product, cart=cart, id=cart_item_id)
+            if cart_item.quantity > 1:
+                cart_item.quantity -= 1
+                cart_item.save()
+            else:
+                cart_item.delete()
+     except:
+        pass
      return redirect('cart')
 
-def remove_cart_item(request, product_id):
+def remove_cart_item(request, product_id, cart_item_id):
      cart = Cart.objects.get(cart_id = _cart_id(request))
      product = get_object_or_404(Product, id=product_id)
-     cart_item = CartItem.objects.get(product=product, cart=cart)
+     cart_item = CartItem.objects.get(product=product, cart=cart, id=cart_item_id)
      cart_item.delete()
      return redirect("cart")
 
--- kiosk/store/admin.py
+++ kiosk/store/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 
-from .models import Product
+from .models import Product, Variation
 # Register your models here.
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ("product_name", "slug", "price", "images", "stock", "category", "is_available", "created_date", "modified_date")
     prepopulated_fields = {"slug": ("product_name",)}
-admin.site.register(Product, ProductAdmin)    
+admin.site.register(Product, ProductAdmin) 
+
+
+class VariationAdmin(admin.ModelAdmin):
+    list_display = ("product", "variation_category", "variation_value", "is_active")
+    list_editable = ("is_active",)
+    list_filter = ("product", "variation_category", "variation_value")
+
+admin.site.register(Variation, VariationAdmin)
