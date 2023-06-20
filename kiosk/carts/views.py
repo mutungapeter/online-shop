f99ca4a28289c0499a7010017f57ac7a6bb1f5dd
peter mutunga <peter.mutunga@oceanscan.co.ke>
2023-06-21 00:01:01 +0300

    fixed cart counter

--- kiosk/carts/views.py
+++ kiosk/carts/views.py
@@ -95,12 +95,13 @@ def remove_cart_item(request, product_id, cart_item_id):
      cart_item.delete()
      return redirect("cart")
 
-def cart(request, total=0, quantity=0, cart_items=None):
+def cart(request, tax=0 ,total=0,  grand_total = 0, quantity=0, cart_items=None):
     try:
-         cart = Cart.objects.get(cart_id=_cart_id(request))
-         cart_items = CartItem.objects.filter(cart=cart, is_active=True)
-         tax = 0  # Assign a default value to tax
-         grand_total = 0  # Assign a default value to tax
+         if request.user.is_authenticated:
+              cart_items = CartItem.objects.filter(user=request.user, is_active=True)
+         else:
+               cart = Cart.objects.get(cart_id=_cart_id(request))
+               cart_items = CartItem.objects.filter(cart=cart, is_active=True)
          for cart_item in cart_items:
               total += (cart_item.product.price * cart_item.quantity)
               quantity += cart_item.quantity
