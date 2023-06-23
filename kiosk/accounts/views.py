008242f2286fc519feb77c57e4eebb4cecdd45d9
peter mutunga <peter.mutunga@oceanscan.co.ke>
2023-06-24 01:24:12 +0300

    cart checkout functionality

--- kiosk/accounts/views.py
+++ kiosk/accounts/views.py
@@ -15,6 +15,7 @@ from django.utils.encoding import force_bytes
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import EmailMessage
 
+import requests
 # Create your views here.
 
 def register(request):
@@ -67,15 +68,52 @@ def login(request):
                 is_cart_item_exists = CartItem.objects.filter(cart=cart).exists()
                 if is_cart_item_exists:
                     cart_item = CartItem.objects.filter(cart=cart)
+                    #getting product variation byb cat it
+                    product_variation = []
                     for item in cart_item:
-                        item.user = user
-                        item.save()
-                         
+                        variation = item.variations.all()
+                        product_variation.append(list(variation))
+
+                    #get cart items from user to access it's product variation
+                    cart_item = CartItem.objects.filter(user=user)
+                    ex_var_list = []
+                    id = []
+                    for item in cart_item:
+                         existing_variation = item.variations.all()
+                         ex_var_list.append(list(existing_variation))
+                         id.append(item.id)
+
+                    for pr in product_variation:
+                        if pr in ex_var_list:
+                            index = ex_var_list.index(pr)
+                            item_id = id[index]
+                            item = CartItem.objects.get(id=item_id)
+                            item.quantity += 1
+                            item.user = user
+                            item.save()
+                        else:
+                            cart_item = CartItem.objects.filter(cart=cart)
+                            for item in cart_item:
+                                item.user = user
+                                item.save()
+                                
             except:
                 pass
             auth.login(request, user)
             messages.success(request, "Login successful")
-            return redirect('dashboard')
+            url = request.META.get('HTTP_REFERER')
+            try:
+                query = requests.utils.urlparse(url).query
+                # print('query ->', query)
+                print("------------------")
+                # next=/cart/checkout/
+                params = dict(x.split('=') for x in query.split('&')) 
+                # print('params ->', params)
+                if 'next' in params:
+                    nextPage = params["next"]
+                    return redirect(nextPage)    
+            except:
+                return redirect('dashboard')
         else:
             messages.error(request, "invalid login credentils")
             return redirect('login')
