9b3fa2e43749c8caeb8e052b340e1adc4f4e59bb
peter mutunga <peter.mutunga@oceanscan.co.ke>
2023-07-05 06:59:10 +0300

    dashboard edits

--- kiosk/accounts/admin.py
+++ kiosk/accounts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-
+from django.utils.html import format_html
 from .models import *
 
 # Register your models here.
@@ -30,3 +30,12 @@ class AccountAdmin(UserAdmin):
     
     
 admin.site.register(Account, AccountAdmin)
+
+
+class UserProfileAdmin(admin.ModelAdmin):
+    def thumbnail(self, object):
+        return format_html('<img src="{}" width="30" style="border-radius:50%;">'.format(object.profile_picture.url) )
+    thumbnail.short_description = 'Profile_picture'
+    list_display = ("thumbnail", "user", "city", "state", "country")
+
+admin.site.register(UserProfile,UserProfileAdmin)
